75924a507b65dd28d761b67ac96cbf528fe6db42
Kaleb Molewe <88307946+KalebMolewe23@users.noreply.github.com>
2024-03-22 14:10:05 +0700

    new logo

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -3,7 +3,8 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title><?= $title ?></title>
-        <link rel="stylesheet" href="{{ asset('assets/frontend/css/style.css') }}">    
+        <link rel="stylesheet" href="{{ asset('assets/frontend/css/style.css') }}"> 
+        <link rel="icon" href="{{ asset('assets/logo/favicon.png') }}" type="image/x-icon">   
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     </head>
@@ -13,7 +14,7 @@
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container">
                 <!-- Logo -->
-                <a class="navbar-brand" href="#">StudioProjectID</a>
+                <a class="navbar-brand" href="#"><img src="{{ asset('assets/logo/logo2.png') }}" style="width:250px;"></a>
                 
                 <!-- Toggle Btn -->
                 <button class="navbar-toggler shadow-none border-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
